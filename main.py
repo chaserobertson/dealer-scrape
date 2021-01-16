63102f29227e33cf777e6ca1a01a35ace64ecebb
Chase Robertson <chaserobertson208@gmail.com>
2021-01-16 12:20:54 -0700

    jk

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ async def get_reviews_page():
     page_num = 1
     request_url = 'https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/page'
     request_url += page_num
-    request_url += '/?filter=ONLY_POSITIVE'
     r = await asession.get(request_url)
     return r
 
