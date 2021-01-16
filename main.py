124e15c398ba78ca4363eff8fa3080dc98e446c7
Chase Robertson <chaserobertson208@gmail.com>
2021-01-16 13:09:23 -0700

    one page of reviews collected and imported

--- main.py
+++ main.py
@@ -14,26 +14,55 @@ async def get_reviews_page():
 
 
 class Review:
-    def __init__(self, rating, text):
+    def __init__(self, rating, content):
         self.rating = rating
-        self.text = text
+        self.content = content
 
     def __str__(self):
-        output = 'Rating: ' + str(self.rating) + '\n'
-        output += self.text + '\n'
+        output = '\n'
+        output += 'Rating: ' + str(self.rating) + '\n'
+        output += self.content + '\n'
         return output
+
+
+class ReviewCollection:
+    def __init__(self):
+        self.reviews = []
+
+    def __str__(self):
+        output = ''
+        for review in self.reviews:
+            output += str(review)
+        return output
+
+    def addReview(self, rating, content):
+        self.reviews.append(Review(rating, content))
+
+    def getReviews(self):
+        return self.reviews
 
 
 def main():
     # get all NUM_PAGES review pages asynchronously
     results = asession.run(get_reviews_page)
+    # initialize review collection
+    review_collection = ReviewCollection()
 
     for result in results:
         # get all review elements in current page
         review_elements = result.html.find('.review-entry')
+
         for re in review_elements:
             # get rating of this review
             rating = re.search(' rating-{} ')[0]
+
+            # get content of this review
+            content = re.find('.review-content')[0].text
+
+            # add review to collection
+            review_collection.addReview(rating, content)
+
+    print(review_collection)
 
 
 main()
