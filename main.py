79f2ba8b13de4cf380ace3dff0e40df47c90cbf3
Chase Robertson <chaserobertson208@gmail.com>
2021-01-16 12:20:13 -0700

    filter reviews by type only positive

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ asession = AsyncHTMLSession()
 
 
 async def get_reviews_page():
-    r = await asession.get('https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/page1')
+    page_num = 1
+    request_url = 'https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/page'
+    request_url += page_num
+    request_url += '/?filter=ONLY_POSITIVE'
+    r = await asession.get(request_url)
     return r
 
 
